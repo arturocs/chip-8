72b40e59c9bf5a8dd3a2305d9cfe17d2375f7335
Arturo <arturocs@protonmail.com>
2021-02-26 19:54:17 +0100

    Converted opcode comments to documentation comments

--- src/chip8.rs
+++ src/chip8.rs
@@ -58,31 +58,31 @@ impl Chip8 {
         self.memory[self.pc as usize + 1]
     }
 
-    // 0x00E0: Clears the screen
+    /// `0x00E0`: Clears the screen
     fn x00E0(&mut self) {
         self.gfx.fill([0; 64]);
         self.draw_flag = true
     }
 
-    // 0x00EE: Returns from subroutine
+    /// `0x00EE`: Returns from subroutine
     fn x00EE(&mut self) {
         self.sp = self.sp.saturating_sub(1);
         self.pc = self.stack[self.sp as usize];
     }
 
-    // 0x1NNN: Jumps to address NNN
+    /// `0x1NNN`: Jumps to address `NNN`
     fn x1NNN(&mut self) {
         self.pc = Self::NNN(self) - 2;
     }
 
-    // 0x2NNN: Calls subroutine at NNN.
+    /// `0x2NNN`: Calls subroutine at `NNN`.
     fn x2NNN(&mut self) {
         self.stack[self.sp as usize] = self.pc;
         self.sp += 1;
         self.pc = Self::NNN(self) - 2;
     }
 
-    // 0x3XNN: Skips the next instruction if VX equals NN
+    /// `0x3XNN`: Skips the next instruction if `VX` equals `NN`
     fn x3XNN(&mut self) {
         let (_, x, ..) = self.opcode;
         if self.v[x as usize] == Self::NN(&self) {
@@ -90,14 +90,14 @@ impl Chip8 {
         }
     }
 
-    // 0x4XNN: Skips the next instruction if VX doesn't equal NN
+    /// `0x4XNN`: Skips the next instruction if `VX` doesn't equal `NN`
     fn x4XNN(&mut self) {
         let (_, x, ..) = self.opcode;
         if self.v[x as usize] != Self::NN(&self) {
             self.pc += 2;
         }
     }
-    // 0x5XY0: Skips the next instruction if VX equals VY.
+    /// `0x5XY0`: Skips the next instruction if `VX` equals `VY`.
     fn x5XY0(&mut self) {
         let (_, x, y, _) = self.opcode;
         if x == y {
@@ -105,43 +105,43 @@ impl Chip8 {
         }
     }
 
-    // 0x6XNN: Sets VX to NN.
+    /// `0x6XNN`: Sets `VX` to `NN`.
     fn x6XNN(&mut self) {
         let (_, x, ..) = self.opcode;
         self.v[x as usize] = Self::NN(&self);
     }
 
-    // 0x7XNN: Adds NN to VX.
+    /// `0x7XNN`: Adds `NN` to `VX`.
     fn x7XNN(&mut self) {
         let (_, x, ..) = self.opcode;
         self.v[x as usize] += Self::NN(&self);
     }
 
-    // 0x8XY0: Sets VX to the value of VY
+    /// `0x8XY0`: Sets `VX` to the value of `VY`
     fn x8XY0(&mut self) {
         let (_, x, y, _) = self.opcode;
         self.v[x as usize] = self.v[y as usize];
     }
 
-    // 0x8XY1: Sets VX to "VX OR VY"
+    /// `0x8XY1`: Sets `VX` to `VX OR VY`
     fn x8XY1(&mut self) {
         let (_, x, y, _) = self.opcode;
         self.v[x as usize] |= self.v[y as usize];
     }
 
-    // 0x8XY2: Sets VX to "VX AND VY"
+    /// `0x8XY2`: Sets `VX` to `VX AND VY`
     fn x8XY2(&mut self) {
         let (_, x, y, _) = self.opcode;
         self.v[x as usize] &= self.v[y as usize];
     }
 
-    // 0x8XY3: Sets VX to "VX XOR VY"
+    /// `0x8XY3`: Sets `VX` to `VX XOR VY`
     fn x8XY3(&mut self) {
         let (_, x, y, _) = self.opcode;
         self.v[x as usize] ^= self.v[y as usize];
     }
 
-    // 0x8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
+    /// `0x8XY4`: Adds `VY` to `VX`. `VF` is set to `1` when there's a carry, and to `0` when there isn't
     fn x8XY4(&mut self) {
         let (_, x, y, _) = self.opcode;
         let (result, overflow) = self.v[x as usize].overflowing_add(self.v[y as usize]);
@@ -149,7 +149,7 @@ impl Chip8 {
         self.v[0xF] = overflow as u8;
     }
 
-    // 0x8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+    /// `0x8XY5`: `VY` is subtracted from `VX`. `VF` is set to `0` when there's a borrow, and `1` when there isn't
     fn x8XY5(&mut self) {
         let (_, x, y, _) = self.opcode;
         let (result, borrow) = self.v[x as usize].overflowing_sub(self.v[y as usize]);
@@ -157,14 +157,14 @@ impl Chip8 {
         self.v[0xF] = borrow as u8;
     }
 
-    // 0x8XY6: Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift
+    /// `0x8XY6`: Shifts `VX` right by one. `VF` is set to the value of the least significant bit of `VX` before the shift
     fn x8XY6(&mut self) {
         let (_, x, ..) = self.opcode;
         self.v[0xF] = self.v[x as usize] & 0x1;
         self.v[x as usize] >>= 1;
     }
 
-    // 0x8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+    /// `0x8XY7`: Sets `VX` to `VY` minus `VX`. `VF` is set to `0` when there's a borrow, and `1` when there isn't
     fn x8XY7(&mut self) {
         let (_, x, y, _) = self.opcode;
         let (result, borrow) = self.v[y as usize].overflowing_sub(self.v[x as usize]);
@@ -172,14 +172,14 @@ impl Chip8 {
         self.v[0xF] = borrow as u8;
     }
 
-    // 0x8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift
+    /// `0x8XYE`: Shifts `VX` left by one. `VF` is set to the value of the most significant bit of `VX` before the shift
     fn x8XYE(&mut self) {
         let (_, x, ..) = self.opcode;
         self.v[0xF] = self.v[x as usize] >> 7;
         self.v[x as usize] <<= 1;
     }
 
-    // 0x9XY0: Skips the next instruction if VX doesn't equal VY
+    /// `0x9XY0`: Skips the next instruction if `VX` doesn't equal `VY`
     fn x9XY0(&mut self) {
         let (_, x, y, _) = self.opcode;
         if self.v[x as usize] != self.v[y as usize] {
@@ -187,23 +187,23 @@ impl Chip8 {
         }
     }
 
-    // ANNN: Sets I to the address NNN
+    /// `ANNN`: Sets I to the address `NNN`
     fn xANNN(&mut self) {
         self.i = Self::NNN(self);
     }
 
-    // BNNN: Jumps to the address NNN plus V0
+    /// `BNNN`: Jumps to the address `NNN` plus `V0`
     fn xBNNN(&mut self) {
         self.i = Self::NNN(self) + self.v[0] as u16 - 2;
     }
 
-    // CXNN: Sets VX to a random number and NN
+    /// `CXNN`: Sets `VX` to a random number and `NN`
     fn xCXNN(&mut self) {
         let (_, x, ..) = self.opcode;
         self.v[x as usize] = thread_rng().gen::<u8>() & Self::NN(self);
     }
 
-    // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
+    /// `DXYN`: Draws a sprite at coordinate `(VX, VY)` that has a width of 8 pixels and a height of `N` pixels.
     fn xDXYN(&mut self) {
         let (_, x, y, n) = self.opcode;
         let x = self.v[x as usize] as usize;
@@ -224,27 +224,27 @@ impl Chip8 {
         }
         self.draw_flag = true;
     }
-    // EX9E: Skips the next instruction if the key stored in VX is pressed
+    /// `EX9E`: Skips the next instruction if the key stored in `VX` is pressed
     fn xEX9E(&mut self) {
         let (_, x, ..) = self.opcode;
         if self.keys[self.v[x as usize] as usize] != 0 {
             self.pc += 2;
         }
     }
-    // EXA1: Skips the next instruction if the key stored in VX isn't pressed
+    /// `EXA1`: Skips the next instruction if the key stored in `VX` isn't pressed
     fn xEXA1(&mut self) {
         let (_, x, ..) = self.opcode;
         if self.keys[self.v[x as usize] as usize] == 0 {
             self.pc += 2;
         }
     }
-    // FX07: Sets VX to the value of the delay timer
+    /// `FX07`: Sets `VX` to the value of the delay timer
     fn xFX07(&mut self) {
         let (_, x, ..) = self.opcode;
         self.v[x as usize] = self.delay_timer;
     }
 
-    // FX0A: A key press is awaited, and then stored in VX
+    /// `FX0A`: A key press is awaited, and then stored in `VX`
     fn xFX0A(&mut self) {
         let (_, x, ..) = self.opcode;
         if let Some((i, _)) = self.keys.iter().enumerate().find(|(_, &k)| k != 0) {
@@ -255,19 +255,19 @@ impl Chip8 {
         }
     }
 
-    // FX15: Sets the delay timer to VX
+    /// `FX15`: Sets the delay timer to `VX`
     fn xFX15(&mut self) {
         let (_, x, ..) = self.opcode;
         self.delay_timer = self.v[x as usize];
     }
 
-    // FX18: Sets the sound timer to VX
+    /// `FX18`: Sets the sound timer to `VX`
     fn xFX18(&mut self) {
         let (_, x, ..) = self.opcode;
         self.sound_timer = self.v[x as usize];
     }
 
-    // FX1E: Adds VX to I
+    /// `FX1E`: Adds `VX` to `I`
     fn xFX1E(&mut self) {
         let (_, x, ..) = self.opcode;
         // Most CHIP-8 interpreters' FX1E instructions do not affect VF, with one exception: The CHIP-8 interpreter for the Commodore
@@ -281,13 +281,13 @@ impl Chip8 {
         self.i += self.v[x as usize] as u16;
     }
 
-    // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
+    /// `FX29`: Sets `I` to the location of the sprite for the character in `VX`. Characters `0-F` (in hexadecimal) are represented by a `4x5` font
     fn xFX29(&mut self) {
         let (_, x, ..) = self.opcode;
         self.i = self.v[x as usize] as u16 * 0x5;
     }
 
-    // FX33: Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
+    /// `FX33`: Stores the Binary-coded decimal representation of `VX` at the addresses `I`, `I + 1`, and `I + 2`
     fn xFX33(&mut self) {
         let (_, x, ..) = self.opcode;
         let i = self.i as usize;
@@ -296,14 +296,14 @@ impl Chip8 {
         self.memory[i + 2] = (self.v[x as usize] % 100) % 10;
     }
 
-    // FX55: Stores V0 to VX in memory starting at address I
+    /// `FX55`: Stores `V0` to `VX` in memory starting at address `I`
     fn xFX55(&mut self) {
         let (_, x, ..) = self.opcode;
         let i = self.i as usize;
         self.memory[i..=i + x as usize].copy_from_slice(&self.v[0..=x as usize]);
         //self.i += x as u16;
     }
-    // FX65: Fills V0 to VX with values from memory starting at address I
+    /// `FX65`: Fills `V0` to `VX` with values from memory starting at address `I`
     fn xFX65(&mut self) {
         let (_, x, ..) = self.opcode;
         let i = self.i as usize;
